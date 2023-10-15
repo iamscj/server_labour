7f37e90d445c2afc28787133a9e3db4ec39af1dd
Shreyasa Joshi <shreyasajoshi.cs20@rvce.edu.in>
2023-10-16 00:30:24 +0530

    Update db.js

--- database/db.js
+++ database/db.js
@@ -3,9 +3,9 @@ const { Pool } = pg;
 
 export const pool = new Pool({
     user: 'admin',
-    host: 'dpg-cimq6i5gkuvotpjfggeg-a.oregon-postgres.render.com',
-    database: 'laboursdb',
-    password: 'n9UJe8afa5pyUNncuJ39MjD2wCt3nVq7',
+    host: 'dpg-ckm2jfgu1l6c73ef7csg-a.oregon-postgres.render.com',
+    database: 'labourdb',
+    password: 'Max6nv4l1ZGM1Y5BAXx3wJ0tDPLcW05z',
     port: '5432',
     ssl: {
         rejectUnauthorized: false,
